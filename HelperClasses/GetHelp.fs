00b175c249814fdbf150b7b2567bc80b00ef8b5b
Simon Bay Nymand <simon.bay@live.dk>
2018-12-03 14:20:40 +0100

    Guess Word and working on GetHelp

--- HelperClasses/GetHelp.fs
+++ HelperClasses/GetHelp.fs
@@ -1,3 +1,9 @@
 ﻿module GetHelp
 
 //Get a helping character based on which parts of the words have or haven't been guessed.
+let HelpLetter(wordguessed : string) (wordToGuss : string) : char =
+
+    //wordguessed.ToCharArray
+   //let wordToGussC = wordToGuss.ToCharArray()
+
+    'f'
