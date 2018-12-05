8bd3eb12e317dbcaa190e58b2024f10499f151a3
Doh09 <alexandreasen09@gmail.com>
2018-12-05 15:18:01 +0100

    Added 2 extra tests to GetHiddenWord to confirm it works as intended regardless of the character used in Hidden. Should work so long as it's not a letter character.

--- XUnitTestProject1/Test_GetHiddenWord.fs
+++ XUnitTestProject1/Test_GetHiddenWord.fs
@@ -47,5 +47,45 @@ let ``Words can be hidden without blanks`` () =
     Assert.Equal(expectedResult3, testResult3)
 
 [<Fact>]
-let ``My test3`` () =
-    Assert.True(true)
+let ``Words can be hidden with blanks - Changed HIDDEN value`` () =
+    Config.HIDDEN <- "!"
+    let testWord1 = "This is a cool bar"
+    let testWord2 = "Merciful heavens"
+    let testWord3 = "Wonderful life"
+    let testResult1 = GetHiddenWord.HideWord(testWord1)
+    let testResult2 = GetHiddenWord.HideWord(testWord2)
+    let testResult3 = GetHiddenWord.HideWord(testWord3)
+    let mutable expectedResult1 = ""
+    for c in testWord1 do 
+        expectedResult1 <- expectedResult1 + Config.HIDDEN
+    let mutable expectedResult2 = ""
+    for c in testWord2 do 
+        expectedResult2 <- expectedResult2 + Config.HIDDEN
+    let mutable expectedResult3 = ""
+    for c in testWord3 do 
+        expectedResult3 <- expectedResult3 + Config.HIDDEN
+    Assert.Equal(expectedResult1, testResult1)
+    Assert.Equal(expectedResult2, testResult2)
+    Assert.Equal(expectedResult3, testResult3)
+
+[<Fact>]
+let ``Words can be hidden without blanks - Changed HIDDEN value`` () =
+    Config.HIDDEN <- "!"
+    let testWord1 = "Bar"
+    let testWord2 = "Merciful"
+    let testWord3 = "Wonderful"
+    let testResult1 = GetHiddenWord.HideWord(testWord1)
+    let testResult2 = GetHiddenWord.HideWord(testWord2)
+    let testResult3 = GetHiddenWord.HideWord(testWord3)
+    let mutable expectedResult1 = ""
+    for c in testWord1 do 
+        expectedResult1 <- expectedResult1 + Config.HIDDEN
+    let mutable expectedResult2 = ""
+    for c in testWord2 do 
+        expectedResult2 <- expectedResult2 + Config.HIDDEN
+    let mutable expectedResult3 = ""
+    for c in testWord3 do 
+        expectedResult3 <- expectedResult3 + Config.HIDDEN
+    Assert.Equal(expectedResult1, testResult1)
+    Assert.Equal(expectedResult2, testResult2)
+    Assert.Equal(expectedResult3, testResult3)
