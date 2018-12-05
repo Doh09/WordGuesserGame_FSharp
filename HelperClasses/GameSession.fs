5773275e749e256453bd31d4e823c9dbf464de22
Doh09 <alexandreasen09@gmail.com>
2018-12-05 22:49:34 +0100

    Commented out a method.

--- HelperClasses/GameSession.fs
+++ HelperClasses/GameSession.fs
@@ -59,19 +59,19 @@ let Game() =
            printfn ""
 
 ///Method used for testing word selection..
-let PrintThreeRandomWords() =
-    printfn "Three words: "
-    let word1 = SelectWordForGame.GetWord()
-    let word2 = SelectWordForGame.GetWord()
-    let word3 = SelectWordForGame.GetWord()
-    let word4 = CheckIfWordFullyGuessed.MakeGuess("Be**")( "Bear")( 'a')
-    let word5 = CheckIfWordFullyGuessed.MakeGuess("Be**")( "Bear")( 'r')
-    let word6 = CheckIfWordFullyGuessed.MakeGuess("Be**")( "Bear")( 'x')
-    printfn ""
-    printfn "Word1: %s" word1
-    printfn "Word2: %s" word2
-    printfn "Word3: %s" word3
-    printfn "Word4: %s" word4
-    printfn "Word5: %s" word5
-    printfn "Word6: %s" word6
+//let PrintThreeRandomWords() =
+//    printfn "Three words: "
+//    let word1 = SelectWordForGame.GetWord()
+//    let word2 = SelectWordForGame.GetWord()
+//    let word3 = SelectWordForGame.GetWord()
+//    let word4 = CheckIfWordFullyGuessed.MakeGuess("Be**")( "Bear")( 'a')
+//    let word5 = CheckIfWordFullyGuessed.MakeGuess("Be**")( "Bear")( 'r')
+//    let word6 = CheckIfWordFullyGuessed.MakeGuess("Be**")( "Bear")( 'x')
+//    printfn ""
+//    printfn "Word1: %s" word1
+//    printfn "Word2: %s" word2
+//    printfn "Word3: %s" word3
+//    printfn "Word4: %s" word4
+//    printfn "Word5: %s" word5
+//    printfn "Word6: %s" word6
     
