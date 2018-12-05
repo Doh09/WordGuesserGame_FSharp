82592c35f3d21f2bb57f794a93ccc8220e807e54
Doh09 <alexandreasen09@gmail.com>
2018-12-05 14:55:06 +0100

    Added XUnit tests for: - CheckIfWordFullyGuessed.fs - GetHelp.fs - SelectWordForGame.fs - GetHiddenWord.fs

---
+++ XUnitTestProject1/Test_SelectWordForGame.fs
@@ -0,0 +1,21 @@
+﻿module Test_SelectWordForGame
+
+open System
+open Xunit
+open WordGuesserGame
+
+[<Fact>]
+let ``Select word without blanks`` () =
+    Config.ALLOW_BLANKS <- false
+    let selectedWord = SelectWordForGame.GetWord()
+    Assert.False(selectedWord.Contains(" "))
+
+[<Fact>]
+let ``Select word with blanks`` () =
+    Config.ALLOW_BLANKS <- true
+    let selectedWord = SelectWordForGame.GetWord()
+    Assert.True(selectedWord.Contains(" "))
+
+[<Fact>]
+let ``My test3`` () =
+    Assert.True(true)
---
+++ XUnitTestProject1/Tests.fs
@@ -0,0 +1,9 @@
+﻿module Tests
+
+open System
+open Xunit
+open WordGuesserGame
+
+[<Fact>]
+let ``My test3`` () =
+    Assert.True(true)
